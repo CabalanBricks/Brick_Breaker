dd22215386a2fae51b4bcf561c1c84ee510d21a8
CabalanBricks <134180284+CabalanBricks@users.noreply.github.com>
2025-02-01 15:21:29 +0800

    Update components.dart

--- lib/src/components/components.dart
+++ lib/src/components/components.dart
@@ -1,4 +1,5 @@
 export 'ball.dart';
 export 'bat.dart';
-export 'brick.dart';                                            // Add this export
+export 'brick.dart';
 export 'play_area.dart';
+export 'power_up_ball.dart';
