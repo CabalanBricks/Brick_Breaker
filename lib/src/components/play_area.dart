bcb5cb9fbbfe51312b0c5b189acc5ea276b82783
CabalanBricks <134180284+CabalanBricks@users.noreply.github.com>
2025-02-01 15:21:08 +0800

    Update play_area.dart

--- lib/src/components/play_area.dart
+++ lib/src/components/play_area.dart
@@ -1,21 +1,34 @@
 import 'dart:async';
-
-import 'package:flame/collisions.dart';                        
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-
 import '../brick_breaker.dart';
 
 class PlayArea extends RectangleComponent with HasGameReference<BrickBreaker> {
   PlayArea()
       : super(
-          paint: Paint()..color = const Color(0xfff2e8cf),
-          children: [RectangleHitbox()],                        
+          paint: Paint()
+            ..color = const Color(0xfff2e8cf)
+            ..style = PaintingStyle.fill,
+          children: [RectangleHitbox()], // Main play area hitbox
         );
 
   @override
   FutureOr<void> onLoad() async {
     super.onLoad();
     size = Vector2(game.width, game.height);
+
+    // Remove position offset (was causing misalignment)
+    position = Vector2.zero();
+
+    // Add border as separate component
+    add(RectangleComponent(
+      size: size,
+      paint: Paint()
+        ..color = Colors.black
+        ..style = PaintingStyle.stroke
+        ..strokeWidth = 8,
+      children: [RectangleHitbox()], // Border hitbox
+    ));
   }
 }
